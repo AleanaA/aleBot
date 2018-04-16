c31409f2d435c088f9206dced3d6f10cdcd6c24c
AleanaA <connerkid200022@gmail.com>
2018-04-16 10:56:09 -0400

    This might be easier

--- mods/commands.py
+++ mods/commands.py
@@ -235,7 +235,7 @@ class Commands(Cog):
                 brief="Show a profile!",
                 aliases=[])
     async def profile(self, ctx, *user: discord.Member):
-        if user == "":
+        if not user:
             user = ctx.message.author
         roles = [role.name for role in user.roles]
         del roles[0]

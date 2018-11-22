af88a934fee09aff3b191d91833245fbc18f15bf
AleanaA <aleanaazure@gmail.com>
2018-11-22 13:26:53 -0500

    But does it work?

--- utils/embed.py
+++ utils/embed.py
@@ -1,12 +1,12 @@
 import asyncio
 import discord
 class Embeds:
-    
+
     def create_embed(self, title, color, message, **field):
         emb = discord.Embed()
         emb.title = title
         emb.description = message
         emb.color = color
         for value in field:
-            emb.add_field(name=field, value=value)
+            emb.add_field(name=value, value=field[value])
         return emb

64b8d69644094633963d36c3bb6050b2042dadd6
AleanaA <aleanaazure@gmail.com>
2018-11-22 20:46:50 -0500

    Finishing touches

--- utils/embed.py
+++ utils/embed.py
@@ -4,9 +4,12 @@ class Embeds:
 
     def create_embed(self, title, color, message, **field):
         emb = discord.Embed()
-        emb.title = title
-        emb.description = message
-        emb.color = color
+        if title not '':
+            emb.title = title
+        if message not '':
+            emb.description = message
+        if color not '':
+            emb.color = color
         for value in field:
             emb.add_field(name=field[value][0], value=field[value][1], inline=field[value][2])
         return emb

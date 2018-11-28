14bc8c81ceb9009840bb23ba6d6165603baa1615
AleanaA <aleanaazure@gmail.com>
2018-11-28 13:27:35 -0500

    tests cog

---
+++ mods/tests.py
@@ -0,0 +1,16 @@
+import asyncio
+import discord
+from discord import Game
+from discord.ext import commands
+from discord.ext.commands import Bot
+from utils.cog import Cog
+
+class Tests(Cog):
+    @commands.command(name="getmsg")
+    async def getmsg(self, ctx, mid):
+        for channel in ctx.guild.text_channels:
+            message = await channel.get_message(mid)
+        await ctx.send(message.content)
+
+def setup(bot):
+    bot.add_cog(Tests(bot))

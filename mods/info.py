3b9799facee00350de504db97186d32c26e11706
AleanaA <aleanaazure@gmail.com>
2019-01-03 08:34:20 -0500

    typo

--- mods/info.py
+++ mods/info.py
@@ -152,7 +152,7 @@ class Info(Cog):
         if user.desktop_status != "offline":
             client = "Desktop"
         if user.desktop_status != "offline" and user.mobile_status != "offline":
-            client = "Desktop and Mobile":
+            client = "Desktop and Mobile"
         if user.web_status != "offline" and user.mobile_status != "offline":
             client = "Web and Mobile":
         if user.desktop_status != "offline" and user.web_status != "offline":

be3cc36283affce6a58f057a2f6bd76602fda55c
AleanaA <aleanaazure@gmail.com>
2019-01-28 22:23:27 -0500

    right, that'd be an issue

--- mods/profile.py
+++ mods/profile.py
@@ -261,7 +261,10 @@ class Profiles:
     async def xplb(self, ctx):
         for userid, dic in self.profiles.items():
             print(userid)
-            print(dic)
+            try:
+                print(dic['xp'])
+            except KeyError:
+                pass
 
 def check_folders():
     if not os.path.exists("data"):

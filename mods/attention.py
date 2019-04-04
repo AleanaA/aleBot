d92ba437aa20ab4eae553b8697bbf45f348cb93c
AleanaA <aleanaazure@gmail.com>
2019-04-04 04:33:47 -0400

    code issue?

--- mods/attention.py
+++ mods/attention.py
@@ -16,12 +16,12 @@ class Attention(commands.Cog):
     @commands.command(name="alert")
 #    @commands.cooldown(1, 900, type=commands.BucketType.user)
     async def alert(self, ctx, *, alert:str):
+        await self.bot.appinfo.owner.send("{} sent an alert:\n{}".format(ctx.message.author, alert))
+        await ctx.send("An alert was sent to the bot owner!\nPlease note, you won't be able to send another alert for 15 minutes!")
         alerttt = {}
         alerttt["value1"] = "{} sent an alert:".format(ctx.message.author)
         alerttt["value2"] = alert
         requests.post("https://maker.ifttt.com/trigger/aleBotnotif/with/key/{}".format(self.ifttt), data=alerttt)
-        await self.bot.appinfo.owner.send("{} sent an alert:\n{}".format(ctx.message.author, alert))
-        await ctx.send("An alert was sent to the bot owner!\nPlease note, you won't be able to send another alert for 15 minutes!")
 
 def setup(bot):
     bot.add_cog(Attention(bot))

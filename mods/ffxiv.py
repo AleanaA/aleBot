cf812b59fdf7bff8015ff8185f44b30e0a349369
AleanaA <aleanaazure@gmail.com>
2019-05-01 03:38:25 -0400

    Fix NoneType error

--- mods/ffxiv.py
+++ mods/ffxiv.py
@@ -13,19 +13,24 @@ class FFXIV(commands.Cog):
 
     @commands.command(name="ffchar")
     async def ffchar(self, ctx, world, *, name):
-        request = requests.get("https://xivapi.com/character/search?name={0}&server={1}".format(name, world)).content.decode('utf8')
-        result = json.loads(request)["Results"][0]
         try:
+            request = requests.get("https://xivapi.com/character/search?name={0}&server={1}".format(name, world)).content.decode('utf8')
+            result = json.loads(request)["Results"][0]
             id = result["ID"]
+            character = json.loads(requests.get("https://xivapi.com/character/{}".format(id)).content.decode('utf8'))["Character"]
+            server = character["Server"]
+            name = character["Name"]
+            portrait = character["Portrait"]
+            avatar = character["Avatar"]
         except Exception:
             request = requests.get("https://xivapi.com/character/search?name={0}&server={1}".format(name, world)).content.decode('utf8')
             result = json.loads(request)["Results"][0]
             id = result["ID"]
-        character = json.loads(requests.get("https://xivapi.com/character/{}".format(id)).content.decode('utf8'))["Character"]
-        server = character["Server"]
-        name = character["Name"]
-        portrait = character["Portrait"]
-        avatar = character["Avatar"]
+            character = json.loads(requests.get("https://xivapi.com/character/{}".format(id)).content.decode('utf8'))["Character"]
+            server = character["Server"]
+            name = character["Name"]
+            portrait = character["Portrait"]
+            avatar = character["Avatar"]
         emb = Embeds.create_embed(self, ctx, "Final Fantasy XIV", None, name)
         emb.set_thumbnail(url=avatar)
         emb.set_image(url=portrait)

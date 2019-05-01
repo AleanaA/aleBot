b110128984aba0de236ca3df96cb18c1eb5eeca2
AleanaA <aleanaazure@gmail.com>
2019-05-01 04:50:58 -0400

    Add Active Job

--- mods/ffxiv.py
+++ mods/ffxiv.py
@@ -10,6 +10,45 @@ from utils.config import Config
 class FFXIV(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.jobs = {
+            1: "Gladiator",
+            2: "Pugilist",
+            3: "Marauder",
+            4: "Lancer",
+            5: "Archer",
+            6: "Conjurer",
+            7: "Thaumaturge",
+            8: "Carpenter",
+            9: "Blacksmith",
+            10: "Armorer",
+            11: "Goldsmith",
+            12: "Leatherworker",
+            13: "Weaver",
+            14: "Alchemist",
+            15: "Culinarian",
+            16: "Miner",
+            17: "Botanist",
+            18: "Fisher",
+            19: "Paladin",
+            20: "Monk",
+            21: "Warrior",
+            22: "Dragoon",
+            23: "Bard",
+            24: "White Mage",
+            25: "Black Mage",
+            26: "Arcanist",
+            27: "Summoner",
+            28: "Scholar",
+            29: "Rogue",
+            30: "Ninja",
+            31: "Machinist",
+            32: "Dark Knight",
+            33: "Astrologian",
+            34: "Samurai",
+            35: "Red Mage",
+            36: "Blue Mage"
+        }
+
 
     @commands.command(name="ffchar")
     async def ffchar(self, ctx, world, *, name):
@@ -26,15 +65,20 @@ class FFXIV(commands.Cog):
             request = requests.get("https://xivapi.com/character/search?name={0}&server={1}".format(name, world)).content.decode('utf8')
             result = json.loads(request)["Results"][0]
             id = result["ID"]
-            character = json.loads(requests.get("https://xivapi.com/character/{}".format(id)).content.decode('utf8'))["Character"]
+            idrequest = requests.get("https://xivapi.com/character/{}".format(id)).content.decode('utf8')
+            character = json.loads(idrequest)["Character"]
             server = character["Server"]
             name = character["Name"]
             portrait = character["Portrait"]
             avatar = character["Avatar"]
-        emb = Embeds.create_embed(self, ctx, "Final Fantasy XIV", None, name)
+            activeclass = character["ActiveClassJob"]
+            activeclassname = activeclass["JobID"]
+            activeclasslevel = activeclass["Level"]
+        emb = Embeds.create_embed(self, ctx, "Final Fantasy XIV", None, server)
         emb.set_thumbnail(url=avatar)
         emb.set_image(url=portrait)
-        emb.set_author(name=server)
+        emb.set_author(name=name)
+        emb.add_field(name="Active Job", value="{0} - Level {1}".format(self.jobs[activeclassname], activeclasslevel), inline=True)
         await ctx.send(embed=emb)
 
 def setup(bot):

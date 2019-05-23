e17f86cfb28c2611757fdfe84ecd85543f527204
Aleana Azure <aleanaazure@gmail.com>
2019-05-23 00:44:03 -0400

    this should do it

--- mods/tags.py
+++ mods/tags.py
@@ -16,7 +16,7 @@ class Tags(commands.Cog):
     def FindURL(self, string): 
         # findall() has been used  
         # with valid conditions for urls in string 
-        url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string) 
+        url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', string) 
         return url 
 
     @commands.command(name="tag")
